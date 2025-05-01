85657a3830d0debb9213bd36af337851af1c0fe3
Lynn Matini <40525115+Lynn-Matini@users.noreply.github.com>
2025-05-01 23:04:25 +0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,15 +8,15 @@ function About() {
         contracts or translating languages with AI, I love using technology to
         make life smoother and smarter.
         <br />
-        Outside of coding, you’ll find me exploring the world of blockchain,
-        dabbling in new AI projects, or planning my next big idea over a good
+        Outside of coding, you’ll find me exploring nature, being a foodie,
+        journalling or planning my next big idea over a great
         playlist.
         <br />
         I'm passionate about making tech more accessible and empowering
         businesses with data-driven solutions.
         <br />
         Right now, I’m excited about growing into AI engineering and Data
-        Analytic roles <br />
+        Analysis roles <br />
         while staying curious, creative and just a little bit obsessed with
         purple.
       </p>
